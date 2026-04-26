860cb45727c0194f3b7d5cdedc31d3a3a043aa71
Ashwera Hasan <syedashwerahasan@gmail.com>
2026-04-26 12:40:50 +0530

    fix: json files

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
-});
+  base: command === "build" ? "/portfolio/" : "/",
+}));
